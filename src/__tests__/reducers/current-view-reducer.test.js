9a8c4232b942ba4008d96b5e1e5921d18d50c8a4
Benjamin McFarland <benrmcfarland@gmail.com>
2021-02-19 09:56:30 -0800

    Add second test failing

--- src/__tests__/reducers/current-view-reducer.test.js
+++ src/__tests__/reducers/current-view-reducer.test.js
@@ -5,4 +5,8 @@ describe('currentViewReducer', () => {
   test('Should return default state', () => {
     expect(currentViewReducer(false, { type: null })).toEqual(false);
   });
+
+  test('Should return the flipped state', () => {
+    expect(currentViewReducer(false, { type: 'TOGGLE_PAGE' })).toEqual(false);
+  });
 });
