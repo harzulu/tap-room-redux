03e1166f920334a0365e2e08fcf9301af1de28c1
Benjamin McFarland <benrmcfarland@gmail.com>
2021-02-19 09:11:34 -0800

    Add second test failing

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -34,4 +34,28 @@ describe('kegListReducer', () => {
   test('Should return the default state', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should add new keg to master keg list', () => {
+    const { name, brand, price, alcoholContent, pints, id } = kegDetail;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      pints: price,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pints: pints,
+        id: id
+      }
+    });
+  });
+
 });
