bccc5fe99fe8017a251991ba6772fae715e81dda
Benjamin McFarland <benrmcfarland@gmail.com>
2021-02-19 10:56:28 -0800

    Add first test failing

---
+++ src/__tests__/reducers/current-keg-reducer.test.js
@@ -0,0 +1,37 @@
+import currentKegReducer from '../../reducers/current-keg-reducer.js';
+
+describe('currentKegReducer', () => {
+  let action;
+  
+  const currentState = {
+    1: {
+      name: "Miller Lite",
+      brand: "MillerCoors",
+      price: 94,
+      alcoholContent: 5,
+      pints: 124,
+      id: 1
+    },
+    2: {
+      name: "Angry Orchard",
+      brand: "Boston Beer Co",
+      price: 120,
+      alcoholContent: 4,
+      pints: 124,
+      id: 2
+    }
+  };
+  
+  const kegDetail = {
+    name: "Miller Lite",
+    brand: "MillerCoors",
+    price: 94,
+    alcoholContent: 5,
+    pints: 124,
+    id: 1
+  };
+
+  test('Should return default state', () => {
+    expect(currentKegReducer({}, { type: nul })).toEqual({});
+  })
+});
