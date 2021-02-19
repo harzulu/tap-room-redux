8649fb71d25d547da812a236091510ab407173c7
Benjamin McFarland <benrmcfarland@gmail.com>
2021-02-19 10:31:17 -0800

    Add second test faililng

--- src/__tests__/reducers/is-editing-reducer.test.js
+++ src/__tests__/reducers/is-editing-reducer.test.js
@@ -3,6 +3,10 @@ import isEditingReducer from '../../reducers/is-editing-reducer.js';
 describe('isEditingReducer', () => {
   
   test('Should return default state', () => {
-    expect(isEditingReducer(true, { type: null })).toEqual(false);
+    expect(isEditingReducer(false, { type: null })).toEqual(false);
+  });
+
+  test('Should returned toggled state', () => {
+    expect(isEditingReducer(false, { type: 'TOGGLE_EDIT' })).toEqual(false);
   });
 });
