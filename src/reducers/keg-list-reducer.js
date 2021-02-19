2c2a06595f4215c37afac5ad1bec95df323f20be
Benjamin McFarland <benrmcfarland@gmail.com>
2021-02-19 08:39:29 -0800

    Add first un-tested reducer

---
+++ src/reducers/keg-list-reducer.js
@@ -0,0 +1,22 @@
+export default (state = {}, action) => {
+  const { name, brand, price, alcoholContent, pints, id } = action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id] : {
+          name : name,
+          brand: brand,
+          price: price,
+          alcoholContent: alcoholContent,
+          pints: pints,
+          id: id
+        }
+      });
+    case 'DELETE_KEG':
+      const newState = { ...state };
+      delete newState[id];
+      return newState;
+    default:
+      return state;
+  }
+}
